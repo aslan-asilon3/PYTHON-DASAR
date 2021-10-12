33357ed1e36b8e0285ab8915e470a0dac58ac143
aslan-asilon3 <sulaslansetiawan3@gmail.com>
2021-10-12 15:28:47 +0100

    add mengekstrak elemen-elemen list dan menampungnya ke dalam variabel

---
+++ list/ekstraklist.py
@@ -0,0 +1,6 @@
+mahasiswa = ['10932', 'aslan', 98]
+npm, nama_mahasiswa, hasil_ujian = mahasiswa
+print('npm: ', npm)
+print('nama mahasiswa: ', nama_mahasiswa)
+print('hasil ujian: ', hasil_ujian)
+print(id(mahasiswa))
